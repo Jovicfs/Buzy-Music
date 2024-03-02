65a94488dfb9a293b81c40b176dca9217d3143c3
João Victor Ferreira Silva <joaovictorferrari4@gmail.com>
2024-03-02 13:33:33 -0300

    'tESTE'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/header.jsx'
+
+function App() {
+  return (
+    <div className="Main-App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
