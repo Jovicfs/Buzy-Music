759d3ab6ca294a564de9c04152f73ad19f31371e
João Victor Ferreira Silva <joaovictorferrari4@gmail.com>
2024-03-03 23:02:31 -0300

    Adicionei o Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/header.jsx'
+import Nav from './components/Nav/Nav.jsx'
 
 function App() {
   return (
     <div className="Main-App">
       <Header/>
+      <Nav/>
     </div>
+    
   )
-}
+} 
 
 export default App
