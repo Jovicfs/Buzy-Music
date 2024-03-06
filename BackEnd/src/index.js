768261a0cb425269bfe4df1d22dc0a911f980469
João Victor Ferreira Silva <joaovictorferrari4@gmail.com>
2024-03-06 19:06:21 -0300

    Alterações

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -8,38 +8,6 @@
  app.use(apiPrefix, router);
 
 
-router.get('/artistas', async (req, res) => {
-  try {
-    const artistas = await Artista.find();
-    res.json(artistas);
-  } catch (error) {
-    res.status(500).json({ message: 'não foi possível se conectar' });
-  }
-});
-
-router.get('/albums',async(req,res)=>{
-  try{
-    const albums = await Album.find();
-    res.json(albums)
-  }
-  catch (error){
-    res.status(500).json({message: 'não foi possivel se conectar'});
-  }
-});
-
-router.get('/musica',async (req,res)=>{
-   try{
-    const musicas = await Musica.find();
-    res.json(musicas)
-   }
-   catch(error){
-    res.status(500).json({message: 'não foi possivel se conectar'});
-   }
-});
-
-
-
-
  app.listen(port, () => {
     mongoose.connect('mongodb+srv://Victor:YbC+t8Bb_&6rvD+@banco.cdh6xoq.mongodb.net/?retryWrites=true&w=majority')
       .then(() => {
